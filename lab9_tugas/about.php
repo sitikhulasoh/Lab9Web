ab6b2957ee2506a4efad7020edb087c02596e4cb
sitikhulasoh <56240533+sitikhulasoh@users.noreply.github.com>
2021-06-08 16:14:59 +0700

    Add files via upload

---
+++ lab9_tugas/about.php
@@ -0,0 +1,11 @@
+<?php require('header.php'); ?>
+
+<div class="container">
+<br/>
+    <h2>Ricky Russs</h2>
+    <p>Samlekum!
+    <br>Saya mahasiswa universitas Pelita Bangsa</p>
+<br/>    
+</div>
+
+<?php require('footer.php'); ?>
---
+++ lab9_tugas/footer.php
@@ -0,0 +1,6 @@
+<footer>
+            <p>&copy; 2021, Ricky Rusandi, 3111910717, TI.19.A.2</p>
+        </footer>
+    </div>
+</body>
+</html>
